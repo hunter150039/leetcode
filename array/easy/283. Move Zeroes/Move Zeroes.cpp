168ce0ee225953c294c99a1860fed644a257156b
hunter150039 <hunter360@qq.com>
2018-05-29 18:06:13 +0800

    Create Move Zeroes.cpp

---
+++ array/easy/283. Move Zeroes/Move Zeroes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0)
+               continue;
+            if(i==j)
+               j++;
+            else
+                nums[j++] = nums[i];
+        }
+        while(j<nums.size()){
+            nums[j++]=0;
+        }
+
+            
+    }
+};
