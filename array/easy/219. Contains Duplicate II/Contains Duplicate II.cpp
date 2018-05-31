a4fdef5459c3b3ed3c0aa98a82dd37ac9cbd9837
hunter150039 <hunter360@qq.com>
2018-05-31 15:45:46 +0800

    Update Contains Duplicate II.cpp

--- array/easy/219. Contains Duplicate II/Contains Duplicate II.cpp
+++ array/easy/219. Contains Duplicate II/Contains Duplicate II.cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        if(k==0 || nums.size()<=1)
+            return false;
+        unordered_map<int,int> counts;
+        //counts[nums[0]]=0;
+        for(int i=0;i<nums.size();++i){
+            if(counts[nums[i]]==0){
+                counts[nums[i]]=i+1;
+                continue;
+            }
+            if(i+1-counts[nums[i]]<=k){
+                return true;
+            }
+            else
+                counts[nums[i]]=i+1;
+        }
+        return false;  
 
+    }
+};
